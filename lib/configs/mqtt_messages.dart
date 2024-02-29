b55a3be608eaf2e5d6d9b6d94ec1e95f36dd208f
filou56 <filouwelle@gmail.com>
2024-02-29 14:04:46 +0100

    linux window size & error handling

--- lib/configs/mqtt_messages.dart
+++ lib/configs/mqtt_messages.dart
@@ -3,8 +3,8 @@ const List<String>MQTT_STAMP_DATA       = ['elzwelle/stopwatch/start',
                                            'elzwelle/stopwatch/finish'];
 const List<String>MQTT_STAMP_NUM_AKN    = ['elzwelle/stopwatch/start/number/akn',
                                            'elzwelle/stopwatch/finish/number/akn'];
-const List<String>MQTT_STAMP_NUM_ERROR  = ['elzwelle/stopwatch/start/number/akn',
-                                           'elzwelle/stopwatch/finish/number/akn'];
+const List<String>MQTT_STAMP_NUM_ERROR  = ['elzwelle/stopwatch/start/number/error',
+                                           'elzwelle/stopwatch/finish/number/error'];
 const List<String>MQTT_STAMP_NUM_PUB    = ['elzwelle/stopwatch/start/number',
                                            'elzwelle/stopwatch/finish/number'];
 

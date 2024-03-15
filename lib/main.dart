a9a6542f5200657a700469f13e3e226333aeb124
filou56 <filouwelle@gmail.com>
2024-03-15 19:46:30 +0100

    cla network selection

--- lib/main.dart
+++ lib/main.dart
@@ -9,21 +9,32 @@ import 'dart:io' show Platform;
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  var conNet = 1; // default to elzwelle 192.168.1.100
+
+  if (Platform.isAndroid) {
+    print("Platform Android");
+    conNet = 2;
+
+  } else if (Platform.isLinux || Platform.isWindows ) {
+    print("Platform Linux");
+    print(args);
+    if (args.contains("hivemq")) {
+      conNet = 2;
+    } else if (args.contains("elzwelle")) {
+      conNet = 1;
+    } else if (args.contains("local")) {
+      conNet = 0;
+    }
+  }
+
   final ModeRadioListSelection mode = ModeRadioListSelection(0,HOME_PAGE_MODE,
       false,const Uuid().v1().replaceAll("-",""));
 
-  var config = MqttConfig(net:1);
+  var config = MqttConfig(net:conNet);
 
   final MqttHandler mqttHandler = MqttHandler(config,mode);
 
   mqttHandler.connect();
-
-  if (Platform.isAndroid) {
-    print("Platform Android");
-  } else if (Platform.isLinux) {
-    print("Platform Linux");
-    print(args);
-  }
 
   print(mode.id);
 
